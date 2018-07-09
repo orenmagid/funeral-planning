231bf7e2984cce212f53edd123e4c5b9d3ec0911
orenmagid <autoimpedicis@gmail.com>
2018-07-09 17:14:45 -0400

    new and edit user forms, etc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,45 @@
 class UsersController < ApplicationController
-  before_action :confirm_logged_in
+
+
+  def new
+    @user = User.new
+
+  end
+
+  def create
+      @user = User.new(user_params)
+
+      if @user.valid?
+          @user.save
+          # log in user
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+      else
+          render :new
+      end
+    end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(
+          :username, :email, :password, :password_confirmation)
+    end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :agents
-  resources :dispositions
   resources :expenses
   resources :funeral_homes
   resources :religions
-  resources :service_types
   resources :users
+
+
+
+  # get '/create_user', to: 'users#new'
+  # post '/create_user', to: 'users#create'
+  # get '/edit_user/:id', to: 'users#edit'
+  # patch '/update_user/:id', to: 'users#update'
+  # get '/users/:id', to: 'users#show', as: 'user'
+
 
 
   get 'access/login'
