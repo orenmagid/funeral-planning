b7bac90e178ba59d2647e9902a1b5e04689f6003
orenmagid <autoimpedicis@gmail.com>
2018-07-09 12:50:01 -0400

    added resources/routes for everything (which we prob don't need)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :agents
+  resources :dispositions
+  resources :expenses
+  resources :funeral_homes
+  resources :religions
+  resources :service_types
+  resources :users
+
+
 end
