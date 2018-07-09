921f8625399e64db5591e8dfe6c4893355ee4df3
github username <github email address>
2018-07-09 14:13:27 -0400

    Added seed data for Expense, ServiceType, Religion, Disposition

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# === DISPOSITION SEEDS ===
+
+disposition_array = ["Burial", "Green Burial", "Burial at Sea", "Cremation", "Donate Body to Science", "Other"]
+
+disposition_array.each do |disposition|
+  Disposition.create(type: disposition)
+end
+
+# === EXPENSE SEEDS ===
+
+#we should probably have a "specify" text field for options in this category
+#specify would live in the Funeral Model?
+expense_array = ["Trust", "Funeral Insurance", "Pre-need through Funeral Home", "Cemetery Plot Purchased", "Other"]
+
+expense_array.each do |type|
+  Expense.create(type: type)
+end
+
+# === SERVICE TYPE SEEDS ===
+
+service_type_array = ["Traditional Funeral", "Viewing Only", "Graveside Only", "Witness Cremation", "Memorial Service", "Casual Gathering", "Other"]
+
+service_type_array.each do |service_type|
+  ServiceType.create(type: service_type)
+end
+
+# === RELIGION SEEDS ===
+
+religion_array = ["Athiest", "Agnostic", "Buddhist", "Christian", "Hindu", "Islamic", "Jain", "Jewish", "Sikh"]
+
+religion_array.each do |name|
+  Religion.create(name: name)
+end
+
+#=== FUNERAL HOME SEEDS ===
