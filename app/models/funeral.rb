3ebcbbb15dd952ed919dc8c71eff5528eb07eef7
orenmagid <autoimpedicis@gmail.com>
2018-07-10 15:33:16 -0400

    most popular funeral homes

--- app/models/funeral.rb
+++ app/models/funeral.rb
@@ -5,13 +5,31 @@ class Funeral < ApplicationRecord
   has_many :users, through: :user_funerals
   accepts_nested_attributes_for :funeral_home, reject_if: proc { |attributes| attributes['name'].blank? }
 
-  def most_popular_funeral_home
-    @funerals = Funeral.all
-    @funerals.each do |funeral|
-      funeral.funeral_home.name
-
+  def self.most_popular_funeral_home
+    funerals = Funeral.all
+    funeral_home_hash = {}
+    funerals.each do |funeral|
+      if funeral_home_hash[funeral.funeral_home]
+        funeral_home_hash[funeral.funeral_home] += 1
+      else
+        funeral_home_hash[funeral.funeral_home] = 1
+      end
     end
-
-
+    funeral_home_hash
   end
+
 end
+
+
+# def self.most_res
+#   neighborhoods = Neighborhood.all
+#   ratio_hash = {}
+#   neighborhoods.each do |neighborhood|
+#     reservation_count = 0
+#       neighborhood.listings.each do |listing|
+#         reservation_count += listing.reservations.count
+#       end
+#       ratio_hash[neighborhood.name] = reservation_count
+#   end
+#   Neighborhood.find_by(name: ratio_hash.max_by{|k,v| v}[0])
+# end
