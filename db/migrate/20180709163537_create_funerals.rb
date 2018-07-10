9369aab06e5df5d0e64eb1bad2a38967ac0a7ca1
orenmagid <autoimpedicis@gmail.com>
2018-07-10 17:19:27 -0400

    changes to new_funeral page, etc.

--- db/migrate/20180709163537_create_funerals.rb
+++ db/migrate/20180709163537_create_funerals.rb
@@ -2,14 +2,21 @@ class CreateFunerals < ActiveRecord::Migration[5.2]
   def change
     create_table :funerals do |t|
       t.string :disposition
-      t.string :disposition_other
+      t.text :disposition_expand
       t.string :officiant
+      t.text :officiant_expand
       t.string :eulogist_1
       t.string :eulogist_2
-      t.string :expense
+      t.text :eulogist_expand
+      t.string :financial_plan
+      t.text :financial_plan_expand
       t.string :service_type
+      t.text :service_type_expand
       t.belongs_to :religion, foreign_key: true
+      t.text :religion_expand
       t.belongs_to :funeral_home, foreign_key: true
+      t.text :funeral_home_expand
+      t.text :general_expand
 
       t.timestamps
     end
---
+++ todo.md
@@ -0,0 +1,18 @@
+Oren:
+* Links back to Menu
+* Stat stuff
+* Add new expand fields to funeral show and overall show pages
+
+August:
+* Refactoring?
+* Stat stuff
+
+
+Overall List:
+* Print Overall Summary to PDF?
+* Email summary to contacts? (compose message)
+* Validations
+
+Wednesday Morning:
+* Add and edit language throughout
+* Add CSS to improve user experience
