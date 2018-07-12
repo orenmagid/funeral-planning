27741418be94ea8ab54c6c23373bfa253503aaf4
orenmagid <autoimpedicis@gmail.com>
2018-07-12 09:16:05 -0400

    Generating Rspec Folders and Files

---
+++ spec/controllers/funeral_homes_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe FuneralHomesController, type: :controller do
+
+end
---
+++ spec/controllers/funerals_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe FuneralsController, type: :controller do
+
+end
