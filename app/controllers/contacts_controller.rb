0f9d068806395d3b6c877ffed19ef7d516cc1398
github username <github email address>
2018-07-12 12:13:05 -0400

    edits

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,7 +16,6 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     @contact.user_id = session[:user_id]
     if @contact.save
-      session[:contacts] << @contact
       redirect_to user_contacts_path(@contact.user)
     else
       render "new"
