9588661558e275772189ea2598ec5e4b83929b33
github username <github email address>
2018-07-13 00:23:17 -0400

    deleted views and modified routes. Added alerts to the flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,15 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
 
   get 'users/summary', to: 'users#summary'
 
-
-  # resources :funeral_homes
-  # resources :religions
   resources :users, param: :username do
     resources :contacts, except: [:show]
     resources :funerals
   end
 
-
-
-
-
-
-
-  # get '/create_user', to: 'users#new'
-  # post '/create_user', to: 'users#create'
-  # get '/edit_user/:id', to: 'users#edit'
-  # patch '/update_user/:id', to: 'users#update'
-  # get '/users/:id', to: 'users#show', as: 'user'
-
-
   get 'home', to: 'users#show', as: :home
-  root "static_pages#home"
 
   get 'access/login'
   post 'access/attempt_login'
